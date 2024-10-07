e786fbea228e2bce7344671a21dcf15df381f457
Ali Muazzam <muazzamali741@gmail.com>
2024-10-07 09:04:49 +0530

    added subarray Sum equal k

--- HashMaps/subarraysSumK.py
+++ HashMaps/subarraysSumK.py
@@ -2,8 +2,16 @@ def subarrays(arr, k):
     answer = 0
     prefixSum = 0
     memo = {}
-    memo[prefixSum] = 0
+    memo[prefixSum] = 1
     for num in nums:
         prefixSum += num
 
-        if
+        if (prefixSum - k) in memo:
+            answer += memo[prefixSum - k]
+
+        if prefixSum in memo:
+            memo[prefixSum] += 1
+        else:
+            memo[prefixSum] = 1
+
+    return answer
